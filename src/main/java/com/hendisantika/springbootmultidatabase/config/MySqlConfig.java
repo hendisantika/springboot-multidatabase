8d15d113f05ee2be2c101a771e3f59544c94585b
Hendi Santika <hendisantika@gmail.com>
2020-02-04 06:49:48 +0700

    #11 - Add mysqlTransactionManager in MySqlConfig

--- src/main/java/com/hendisantika/springbootmultidatabase/config/MySqlConfig.java
+++ src/main/java/com/hendisantika/springbootmultidatabase/config/MySqlConfig.java
@@ -8,9 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -45,6 +48,12 @@ public class MySqlConfig {
     public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(@Qualifier("mysqlDataSource") DataSource hubDataSource, EntityManagerFactoryBuilder builder) {
         return builder.dataSource(hubDataSource).packages("com.sma.backend.multidb.database.mysql.domain")
                 .persistenceUnit("mysql").build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager mysqlTransactionManager(@Qualifier("mysqlEntityManagerFactory") EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
     }
 
 }
