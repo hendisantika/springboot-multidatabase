3afd82a811caf6673c026efd36c9a5bd0281102c
Hendi Santika <hendisantika@gmail.com>
2020-02-04 06:52:01 +0700

    #16 - Add sqlServerTransactionManager in SqlServerConfig

--- src/main/java/com/hendisantika/springbootmultidatabase/config/SqlServerConfig.java
+++ src/main/java/com/hendisantika/springbootmultidatabase/config/SqlServerConfig.java
@@ -7,9 +7,12 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -45,6 +48,11 @@ public class SqlServerConfig {
                 .packages("com.sma.backend.multidb.database.sqlserver.domain")
                 .persistenceUnit("sqlserver")
                 .build();
+    }
 
+    @Bean
+    public PlatformTransactionManager sqlServerTransactionManager(@Qualifier("sqlServerEntityManagerFactory")
+                                                                          EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
     }
 }
