99b7db59cd3270d47d19def53977633b620144ab
Hendi Santika <hendisantika@gmail.com>
2020-02-04 06:51:17 +0700

    #15 - Add sqlServerEntityManagerFactory in SqlServerConfig

--- src/main/java/com/hendisantika/springbootmultidatabase/config/SqlServerConfig.java
+++ src/main/java/com/hendisantika/springbootmultidatabase/config/SqlServerConfig.java
@@ -3,9 +3,11 @@ package com.hendisantika.springbootmultidatabase.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -34,5 +36,15 @@ public class SqlServerConfig {
     @Bean
     public DataSource sqlServerDataSource(@Qualifier("sqlServerDataSourceProperties") DataSourceProperties dataSourceProperties) {
         return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "sqlServerEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean sqlServerEntityManagerFactory(@Qualifier("sqlServerDataSource") DataSource sqlServerDataSource, EntityManagerFactoryBuilder builder) {
+
+        return builder.dataSource(sqlServerDataSource)
+                .packages("com.sma.backend.multidb.database.sqlserver.domain")
+                .persistenceUnit("sqlserver")
+                .build();
+
     }
 }
