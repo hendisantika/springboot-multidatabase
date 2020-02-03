75f487e8ef0c77d1e024648fbcf4fc3945999b2f
Hendi Santika <hendisantika@gmail.com>
2020-02-04 06:41:26 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmultidatabase/SpringbootMultidatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultidatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultidatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultidatabaseApplication.class, args);
+	}
+
+}
